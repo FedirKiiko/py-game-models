52a465452f6d563d1c79c214dc97b20a0d948e1e
Fedir Kiiko <fedir1357@gmail.com>
2026-09-09 18:37:04 +0300

    related_name added

--- db/models.py
+++ db/models.py
@@ -12,7 +12,7 @@ class Skill(models.Model):
         "This field describes what kind of bonus players can get from it",
         max_length=255
     )
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="skills")
 
 
 class Guild(models.Model):
@@ -24,6 +24,6 @@ class Player(models.Model):
     nickname = models.CharField(unique=True, max_length=255)
     email = models.EmailField(max_length=255)
     bio = models.CharField(max_length=255)
-    race = models.ForeignKey(Race, on_delete=models.CASCADE)
-    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE, related_name="players")
+    guild = models.ForeignKey(Guild, on_delete=models.SET_NULL, null=True, related_name="players")
     created_at = models.DateTimeField(auto_now_add=True)
